d905f07a6712c8141ef455be9366035c8d379584
Stanislav Semenov <stanislav.semenov@tabby.ai>
2023-02-15 13:34:31 +0400

    1.0.11

--- bin/index.js
+++ bin/index.js
@@ -3,15 +3,186 @@
 const dotenv = require('dotenv');
 const _ = require('lodash');
 const axios = require('axios');
-const fs = require('fs').promises;
+const fs = require('fs');
+const fsp = require('fs').promises;
 const path = require('path');
 const FormData = require('form-data');
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 const appRoot = require('app-root-path');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const ejs = require('ejs');
+const shell = require('shelljs');
+
+const SKIP_FILES = ['node_modules', '_tmp.gitignore'];
+const CURR_DIR = process.cwd();
+
+function createDirectoryContents(templatePath, folderName, data) {
+    const filesToCreate = fs.readdirSync(templatePath);
+
+    filesToCreate.forEach(file => {
+        const origFilePath = path.join(templatePath, file);
+
+        // get stats about the current file
+        const stats = fs.statSync(origFilePath);
+
+        if (SKIP_FILES.indexOf(file) > -1) return;
+
+        if (stats.isFile()) {
+            let contents = fs.readFileSync(origFilePath, 'utf8');
+
+            contents = ejs.render(contents, data);
+
+            const writePath = path.join(CURR_DIR, folderName, file);
+            fs.writeFileSync(writePath, contents, 'utf8');
+        } else if (stats.isDirectory()) {
+            fs.mkdirSync(path.join(CURR_DIR, folderName, file));
+
+            // recursive call
+            createDirectoryContents(path.join(templatePath, file), path.join(folderName, file), data);
+        }
+    });
+}
+
+function createFolder(folderName) {
+    if (fs.existsSync(folderName)) {
+        console.log(chalk.red(`Folder ${folderName} exists. Delete or use another name.`));
+        return false;
+    }
+
+    fs.mkdirSync(folderName);
+    return true;
+}
+
+function postProcessNode(folderName) {
+    console.log(chalk.green(`
+    project "${chalk.white.bold(folderName)}" is created!
+    
+    ${chalk.white.bold(`See examples of real plugins here`)}
+    https://github.com/bookletix/plugin-samples
+    ${chalk.gray(`(it will help you understand more quickly)`)}
+    
+    ${chalk.blue.bold('>')} ${chalk.yellow.bold('run')} npm install
+    `));
+
+    shell.cd(folderName);
+
+    let cmd = '';
+   if (shell.which('npm')) {
+        cmd = 'npm install';
+    }
+
+    if (cmd) {
+        const result = shell.exec(cmd);
+
+        if (result.code !== 0) {
+            return false;
+        }
+    } else {
+        console.log(chalk.red('No npm found. Cannot run installation.'));
+    }
+
+    return true;
+}
 
 yargs(hideBin(process.argv))
-    .command('publish', 'publish plugin', (yargs) => {
+    .command('init', 'initial new plugin', (yargs) => {
+        return yargs
+    }, (argv) => {
+        if(_.isNil(argv._[1])) {
+            console.log(`
+    ${chalk.red.bold('Error')}: ${chalk.red('The project name must be set as the first argument of the')} ${chalk.white.bold('init')} ${chalk.red('command.')}
+    ${chalk.white.bold('example:')} ${chalk.gray('bx-cli init my-first-plugin')}
+            `);
+            return;
+        }
+
+        if (!createFolder(argv._[1])) {
+            return;
+        }
+
+        // print logo
+        console.log(logo())
+
+        const CHOICES = fs.readdirSync(path.join(__dirname, 'templates'));
+        const QUESTIONS = [
+            {
+                name: 'name',
+                type: 'string',
+                message: 'Plugin name:',
+            },
+            {
+                name: 'template',
+                type: 'list',
+                message: 'What plugin template would you like to generate?',
+                choices: CHOICES,
+            },
+            {
+                name: 'description',
+                type: 'input',
+                message: 'Plugin description:',
+                default: 'default plugin description',
+            },
+            {
+                name: 'answerRequired',
+                type: 'boolean',
+                message: 'Is answer required?',
+                validate: (input) => {
+                    if (/^(true|false)$/.test(input)) return true;
+                    else return 'put true or false';
+                },
+                default: false,
+            },
+            {
+                name: 'bookletixEmail',
+                type: 'string',
+                validate: (input) => {
+                    if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(input)) return true;
+                    else return 'You entered a wrong email';
+                },
+                message: 'Bookletix account login(email):',
+            },
+            {
+                name: 'bookletixPassword',
+                type: 'password',
+                message: 'Bookletix account password:',
+            },
+        ];
+
+        inquirer.prompt(QUESTIONS)
+            .then(answers => {
+                answers = Object.assign({}, answers, argv);
+
+                const folderName = argv._[1];
+                const pluginTemplate = answers['template'];
+                const templPath = path.join(__dirname, 'templates', pluginTemplate);
+                const pluginDescription = answers['description'];
+                const pluginName = answers['name'];
+                const answerRequired = answers['answerRequired'];
+                const bookletixEmail = answers['bookletixEmail'];
+                const bookletixPassword = answers['bookletixPassword'];
+
+                createDirectoryContents(templPath, folderName, {
+                    pluginName,
+                    folderName,
+                    pluginTemplate,
+                    pluginDescription,
+                    answerRequired,
+                    bookletixEmail,
+                    bookletixPassword,
+                });
+
+                if (!postProcessNode(folderName)) {
+                    return;
+                }
+            });
+    })
+    .parse()
+
+// publish flow
+yargs(hideBin(process.argv))
+    .command('publish', 'publish plugin on bookletix.com', (yargs) => {
         return yargs
     }, (argv) => {
         const result = dotenv.config();
@@ -33,6 +204,8 @@ yargs(hideBin(process.argv))
         if (_.isNil(email) || _.isNil(password)) {
             throw "email or password is required";
         }
+
+        console.log(logo())
 
         publish(email, password);
     })
@@ -66,7 +239,7 @@ function publish(email, password) {
 
     async function readContent(fileName) {
         let file = path.join(appRoot.toString(), fileName);
-        return await fs.readFile(file, {encoding: 'utf-8'}).then((data) => {
+        return await fsp.readFile(file, {encoding: 'utf-8'}).then((data) => {
             return data.toString()
         });
     }
@@ -116,7 +289,7 @@ function publish(email, password) {
     }
 
     async function uploadIcon(filename, accessToken) {
-        const image = await fs.readFile(filename);
+        const image = await fsp.readFile(filename);
         const form = new FormData();
         form.append('file', image, filename);
 
@@ -150,7 +323,8 @@ function publish(email, password) {
     }
 
     if (_.isNil(process.env.EMAIL) || _.isNil(process.env.PASSWORD)) {
-        throw "There is no email or password field in the .env file";
+        console.log(chalk.red(`There is no email or password field in the .env file`));
+        return;
     }
 
     let authFlow = async (accessToken) => {
@@ -186,14 +360,41 @@ function publish(email, password) {
             state: state
         }
 
+        let t = '\x1b[37mStatus\x1b[0m'
+        let tID = '\x1b[37mID\x1b[0m'
+        let tName = '\x1b[37mPlugin\x1b[0m'
+
         getPluginID(manifest.name).then((id) => {
             if (!_.isNil(id)) {
-                updatePlugin(id, plugin).then(() => console.log("success update"));
+                let sText = "success update"
+                let tStatus = '\x1b[32m'+sText+'\x1b[0m'
+                updatePlugin(id, plugin).then(() => console.log('\x1b[36m%s\x1b[0m',`
+        ${tID}: ${id}
+        ${tName}: ${manifest.name}
+        ${t}: ${tStatus}
+`));
             } else {
-                createPlugin(plugin).then(() => console.log("success create"));
+                let sText = "success create"
+                let tStatus = '\x1b[34m'+sText+'\x1b[0m'
+                createPlugin(plugin).then(() => console.log('\x1b[36m%s\x1b[0m',`
+        ${tName}: ${manifest.name}
+        ${t} ${tStatus}
+`));
             }
         });
     }
 
     login(email, password).then(authFlow);
+}
+
+function logo() {
+    const pur = chalk.hex('#E77194').bold
+    return `${chalk.hex('#9451e6').bold(`
+    ▗                   ▗      ▗▗              ${pur('▖▖')}           
+    ▌▌            ▖▄▗▗  ▚▜     ▐▐         ▖▌   ${pur('▘')}            
+    ▌▙▐▐▗▖    ▗ ▗▜▝ ▘▌▌ ▚▚  ▄▄ ▐▐  ▗▖▌▄▖ ▄▚▜▄▖${pur('▗▗▖ ▄▗  ▖▖')}    
+    ▞▞  ▝▞▖▗▐▞▘▀▞▞▖  ▞▞ ▌▙▗▚▘  ▐▞▖▗▚▘▗ ▌▌ ▞▄   ${pur('▌▖  ▚▗▚▝')}     
+    ▌▙  ▝▞▌▐▐   ▚▚▚▖▞▞▝ ▞▞▝▞▄  ▞▞ ▗▚▀ ▘▘  ▞▞   ${pur('▌▖ ▗▗▚▝▖')}     
+    ▚▘▚▚▜▝  ▌▌▖▖▌▌ ▝    ▞▞ ▝▝▜ ▐▐  ▘▜▐▐▝▘ ▝▞▌▄${pur('▝▖▘ ▚▘  ▘▌')}    
+             ▘▘▘▘`)}`
 }
---
+++ bin/templates/webpack-svelte/README.md
@@ -0,0 +1,15 @@
+### Install
+
+To create a new project based on this template using [degit](https://github.com/Rich-Harris/degit):
+```bash
+npx degit bookletix/plugin-samples/webpack-svelte new-plugin
+cd new-plugin
+```
+
+## Publish Setting
+To publish the plugin use bx-cli to set up access to bookletix.com specify the email and password in the .env file
+Example:
+```dotenv
+EMAIL: "..."
+PASSWORD: "..."
+```
