5042deba695a69fab7a687ff38f3bb29e66d41cb
Stanislav Semenov <stanislav.semenov@tabby.ai>
2023-02-15 13:35:12 +0400

    1.0.12

--- bin/templates/webpack-react/webpack.config.js
+++ bin/templates/webpack-react/webpack.config.js
@@ -30,7 +30,6 @@ module.exports = (env, argv) => ({
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"],
             },
-            // Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
             // { test: /\.(png|jpg|gif|webp|svg|zip)$/, loader: [{ loader: 'url-loader' }] }
             {
                 test: /\.svg/,
